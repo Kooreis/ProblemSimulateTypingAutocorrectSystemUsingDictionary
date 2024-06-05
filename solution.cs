75f43674e58d9337d42c8df7864ad587aaee3b07
Kooreis <kooreis.official@gmail.com>
2024-06-05 09:58:47 -0500

    In this update, a dictionary is created to map common misspellings to their correct spellings.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple console application in C# that simulates a typing autocorrect system using a dictionary:
-
-```C#
 using System;
 using System.Collections.Generic;
 
@@ -15,28 +12,5 @@ class Program
             { "tihng", "thing" },
             { "youre", "you're" }
         };
-
-        Console.WriteLine("Enter a sentence:");
-        string input = Console.ReadLine();
-
-        string[] words = input.Split(' ');
-
-        for (int i = 0; i < words.Length; i++)
-        {
-            if (dictionary.ContainsKey(words[i]))
-            {
-                words[i] = dictionary[words[i]];
-            }
-        }
-
-        string correctedSentence = string.Join(" ", words);
-
-        Console.WriteLine("Corrected sentence:");
-        Console.WriteLine(correctedSentence);
-
-        Console.ReadKey();
     }
 }
-```
-
-This program takes a sentence as input, splits it into words, and checks each word against a dictionary of common misspellings. If a word is found in the dictionary, it is replaced with the correct spelling. The corrected sentence is then displayed.
