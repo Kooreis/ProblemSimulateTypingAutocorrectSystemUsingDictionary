6bc94e526ac4d6f28a506c13faf6683adc4a91a3
Kooreis <kooreis.official@gmail.com>
2024-06-06 09:58:47 -0500

    In this update, the program prompts the user to enter a sentence and reads the user's input.

--- solution.cs
+++ solution.cs
@@ -1,16 +1,2 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dictionary<string, string> dictionary = new Dictionary<string, string>
-        {
-            { "hte", "the" },
-            { "adn", "and" },
-            { "tihng", "thing" },
-            { "youre", "you're" }
-        };
-    }
-}
+Console.WriteLine("Enter a sentence:");
+string input = Console.ReadLine();
