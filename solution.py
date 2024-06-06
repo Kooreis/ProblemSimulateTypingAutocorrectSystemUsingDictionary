d6d653c5be4d831fc11b4e8263027a245283163b
Kooreis <kooreis.official@gmail.com>
2024-06-06 09:58:47 -0500

    Created the Autocorrect class with a constructor that takes a dictionary as an argument and a 'correct' method that uses the 'get_close_matches' function from the 'difflib' library to find the closest match to the input word from the dictionary.

--- solution.py
+++ solution.py
@@ -1,1 +1,6 @@
-import difflib
+class Autocorrect:
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def correct(self, word):
+        return difflib.get_close_matches(word, self.dictionary, n=1, cutoff=0.6)[0]
