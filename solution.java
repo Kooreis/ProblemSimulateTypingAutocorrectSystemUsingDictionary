a46da12bd9123c7da6fbd152f2fee26313e352b7
Kooreis <kooreis.official@gmail.com>
2024-06-06 09:58:47 -0500

    This update introduces the Trie class, which includes a root TrieNode, a constructor to initialize the root, an insert method to add words to the Trie, and a search method to check if a word exists in the Trie.

--- solution.java
+++ solution.java
@@ -1,7 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
+class Trie {
+    private TrieNode root;
 
-class TrieNode {
-    Map<Character, TrieNode> children = new HashMap<>();
-    boolean endOfWord;
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for (char l: word.toCharArray()) {
+            current = current.children.computeIfAbsent(l, c -> new TrieNode());
+        }
+        current.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+        for (char l: word.toCharArray()) {
+            TrieNode node = current.children.get(l);
+            if (node == null) {
+                return false;
+            }
+            current = node;
+        }
+        return current.endOfWord;
+    }
 }
